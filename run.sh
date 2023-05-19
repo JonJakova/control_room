eefa8ea00ad4220ed3a1c36173788a0bfc94c3dd
Jon Jakova <jon.jakova@gmail.com>
2023-05-19 15:36:25 +0200

    fix: deno deploy

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # deno run --allow-net --allow-read --allow-write --allow-env --allow-run --unstable --import-map=import_map.json --config=tsconfig.json src/index.ts
 
-deno run --allow-net --allow-read server.ts
+deno run --allow-env  --allow-net --allow-read server.ts
--- security/encryption.ts
+++ security/encryption.ts
@@ -1,10 +1,18 @@
 import { bcrypt } from "../dependecies.ts";
 
-export const hash = async (password: string) => {
+export const hash = async (password: string) => await bcryp_hash_sync(password);
+export const compare = async (password: string, hash: string) => bcryp_compare_sync(password, hash);
+
+// Bcrypt async implementation
+const bcryp_hash_async = async (password: string) => {
   const salt = await bcrypt.genSalt(8);
   return await bcrypt.hash(password, salt);
 };
+const bcryp_compare_async = async (password: string, hash: string) => await bcrypt.compare(password, hash);
 
-export const compare = async (password: string, hash: string) => {
-  return await bcrypt.compare(password, hash);
+// Bcrypt sync implementation
+const bcryp_hash_sync = async (password: string) => {
+  const salt = await bcrypt.genSalt(8);
+  return bcrypt.hashSync(password, salt);
 };
+const bcryp_compare_sync = (password: string, hash: string) => bcrypt.compareSync(password, hash);
